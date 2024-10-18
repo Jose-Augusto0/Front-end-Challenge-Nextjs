3e24ec3891a8fe4bcf7cbe098e799db327365b67
José Augusto <joseaugustogomessantos12@gmail.com>
2024-10-18 17:04:36 -0300

    estrutura api

--- src/app/pages/api/data.js
+++ src/app/pages/api/data.js
@@ -1,14 +1,57 @@
+// src/app/api/items.js
 import fs from 'fs';
 import path from 'path';
 
+const filePath = path.join(process.cwd(), 'db.json');
+
+const readData = () => {
+  const data = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(data);
+};
+
+const writeData = (data) => {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+};
+
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'db.json');
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).json({ error: 'Erro ao ler o arquivo.' });
-      return;
-    }
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(JSON.parse(data));
-  });
+  switch (req.method) {
+    case 'GET':
+
+      const data = readData();
+      res.status(200).json(data);
+      break;
+
+    case 'POST':
+      const newItem = req.body;
+      const currentData = readData();
+      currentData.push(newItem);
+      writeData(currentData);
+      res.status(201).json(newItem);
+      break;
+
+    case 'PUT':
+      const updatedItem = req.body;
+      const dataToUpdate = readData();
+      const index = dataToUpdate.findIndex(item => item.id === updatedItem.id);
+      if (index !== -1) {
+        dataToUpdate[index] = updatedItem;
+        writeData(dataToUpdate);
+        res.status(200).json(updatedItem);
+      } else {
+        res.status(404).json({ message: 'Item não encontrado' });
+      }
+      break;
+
+    case 'DELETE':
+      const { id } = req.body;
+      const dataToDelete = readData();
+      const filteredData = dataToDelete.filter(item => item.id !== id);
+      writeData(filteredData);
+      res.status(204).end(); 
+      break;
+
+    default:
+      res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+      res.status(405).end(`Método ${req.method} não permitido`);
+  }
 }
